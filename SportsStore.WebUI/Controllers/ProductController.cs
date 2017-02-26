4a48d0b5f102890efee5414e5569c90a59de045f
JoeHyuga <tongtng661@qq.com>
2017-02-26 21:52:10 +0800

    产品目录

---
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using SportStore.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository respository;
+        public ProductController(IProductRepository productRepository)
+        {
+            this.respository = productRepository;
+        }
+        public ViewResult List()
+        {
+            return View(respository.Products);
+        }
+    }
+}
---
+++ SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Ninject;
+using System.Web.Mvc;
+using Moq;
+using SportStore.Domain.Abstract;
+using SportStore.Domain.Entities;
+
+namespace SportsStore.WebUI.Infrastructure
+{
+    public class NinjectDependencyResolver: IDependencyResolver
+    {
+        private IKernel kernel;
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+
+        object IDependencyResolver.GetService(Type serviceType)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerable<object> IDependencyResolver.GetServices(Type serviceType)
+        {
+            throw new NotImplementedException();
+        }
+        private void AddBindings()
+        {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m=>m.Products).Returns(new List<Product> {
+                new Product { Name="Football",Price=25},
+                new Product { Name="Surf board",Price=179},
+                new Product { Name="Running shone",Price=95}
+            });
+            kernel.Bind<IProductRepository>().ToConstant(mock.Object);
+        }
+    }
+}
